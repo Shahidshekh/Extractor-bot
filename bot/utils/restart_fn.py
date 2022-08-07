37176bd80ef3d4e2818dd7b5fef41bc618e871cd
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-08-07 08:11:26 -0700

    Create restart_fn.py

---
+++ bot/utils/restart_fn.py
@@ -0,0 +1,20 @@
+from utils.telegram_helper import clean_all, sendMessage
+from subprocess import run as srun
+from os import execl as osexecl
+
+
+
+
+
+
+def restart(update, context):
+    restart_message = sendMessage("Restarting...", context.bot, update.message)
+    if Interval:
+        Interval[0].cancel()
+    clean_all()
+    srun(["pkill", "-f", "gunicorn|aria2c|qbittorrent-nox"])
+    srun(["python3", "update.py"])
+    with open(".restartmsg", "w") as f:
+        f.truncate(0)
+        f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
+    osexecl(executable, executable, "-m", "bot")
