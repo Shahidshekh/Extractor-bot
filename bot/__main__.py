c65d20aa45594930967bcf0e54f845106ffe6bf0
Shahidshekh <100508780+Shahidshekh@users.noreply.github.com>
2022-08-07 08:22:31 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,16 +1,24 @@
+from pyrogram.handlers import MessageHandler
+from utils.restart_fn import restart
+from bot import RESTART_COMMAND
 
 
-
-
-
-def restart(update, context):
-    restart_message = sendMessage("Restarting...", context.bot, update.message)
-    if Interval:
-        Interval[0].cancel()
-    clean_all()
-    srun(["pkill", "-f", "gunicorn|aria2c|qbittorrent-nox"])
-    srun(["python3", "update.py"])
-    with open(".restartmsg", "w") as f:
-        f.truncate(0)
-        f.write(f"{restart_message.chat.id}\n{restart_message.message_id}\n")
-    osexecl(executable, executable, "-m", "bot")
+if __name__ == "__main__":
+    # create download directory, if not exist
+    if not os.path.isdir(DOWNLOAD_LOCATION):
+        os.makedirs(DOWNLOAD_LOCATION)
+    # Starting The Bot
+    app.start()
+    
+    restart_com = MessageHandler(
+        restart,
+        filters=filters.command([RESTART_COMMAND]),
+    )
+    app.add_handler(restart_command)
+    
+    
+    logging.info(f"@{(app.get_me()).username} Bot Started! 😎")
+    
+    idle()
+    
+    app.stop()
